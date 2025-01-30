722b3c7e10abd430e7940b3cabfb5a7eb6895f8a
Dearbhla <dearbhlalane@gmail.com>
2025-01-30 16:18:19 +0000

    loops2

--- loops/loops.gd
+++ loops/loops.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	queue_redraw()
 	pass
 
 func _draw() -> void:
@@ -22,6 +23,18 @@ func _draw() -> void:
 	for i in range(num_lines):
 		draw_line(Vector2(i * w, 0), Vector2(i * w, s.y), Color.PINK)
 	
+	var num_h_lines = 10
+	var gap = 50
+	var border = 100
+	
+	for i in range(num_h_lines):
+		draw_line(Vector2(100,border + gap * i), Vector2(v.size.x - border, border + gap * i), Color.ALICE_BLUE)
+	
+	for i in range(100):
+		draw_line(Vector2(0, gap * i), Vector2(v.size.x, v.size.y - i * gap), Color.PALE_VIOLET_RED)
+	
+	for i in range(100):
+		draw_line(Vector2(v.size.x, v.size.y - i * gap), Vector2(0, gap * i), Color.PALE_VIOLET_RED)
 	
 	var basket = ["apple","banana","carrot"]
 	
@@ -30,6 +43,23 @@ func _draw() -> void:
 	
 	for i in range(basket.size()):
 		print(basket[i])
+	
+	
+	
+	var m = get_global_mouse_position()
+	print(m)
+	
+	var num_c_lines = m.x / 2
+	
+	gap = v.size.y / num_c_lines
+	
+	for i in range(num_c_lines):
+		draw_line(Vector2(0, i * gap), Vector2(v.size.x, v.size.y - (i * gap)), Color.GREEN_YELLOW)
+	
+	var numCircles = 10
+	
+	for i in range(numCircles):
+		draw_circle(Vector2(i * w + (border / 2), 200 + (border / 2)), 60, Color.PINK)
 	
 	
 	pass
